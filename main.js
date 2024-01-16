9677cbd736acf72d7209be3fa7c2862e2a775ca1
christy cooper betty <ccooper.prsvr@gmail.com>
2024-01-16 11:36:29 -0600

    second commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let heading = document.querySelector("#h1")
+heading.style.color = "aqua"
+heading.style.height = "42px"
+
+let paragraph = document.querySelector(".p-lorem")
+paragraph.innerHTML += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
+
+let list = document.querySelector("ul")
+list.style.opacity = 30
+list.innterHTML += "<li>list17<li>"
+
+let image = document.querySelector()
+image.innerHTML += image.src = "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc="
+image.style.height = "250px"
+image.innerHTML += image.src ="https://depositphotos.com/vector/vector-illustration-yellow-smile-17425351.html"
+image.style.height = "280px"
