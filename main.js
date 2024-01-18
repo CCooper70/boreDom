0589323115856385e20c081afcdbe258ba524f3e
christy cooper betty <ccooper.prsvr@gmail.com>
2024-01-17 19:00:01 -0600

    mostly done with help

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-let heading = document.querySelector("#h1")
-heading.style.color = "aqua"
-heading.style.height = "42px"
+let heading = document.querySelector("h1")
+heading.style.fontSize = "42px";
 
-let paragraph = document.querySelector(".p-lorem")
-paragraph.innerHTML += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
+let paragraph = document.querySelector("p")
+paragraph.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
+paragraph.style.color = "aqua";
 
 let list = document.querySelector("ul")
-list.style.opacity = 30
-list.innterHTML += "<li>list17<li>"
+document.getElementById("item-13").style.opacity = 0.3;
+document.getElementById("item-13").innerText = "Greetings!";
+list.innerHTML += "<li class = 'item'>17</li>";
 
-let image = document.querySelector()
-image.innerHTML += image.src = "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc="
-image.style.height = "250px"
-image.innerHTML += image.src ="https://depositphotos.com/vector/vector-illustration-yellow-smile-17425351.html"
-image.style.height = "280px"
+let image = document.querySelector("img")
+image[0].src = "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc="
+image[0].style.height = "250px"
+
+let image2 =document.querySelector(".imgDiv")
+imgDiv.innerHTML += "https://depositphotos.com/vector/vector-illustration-yellow-smile-17425351.html"
+imgDiv.style.height = "280px"
+
+let listItem = document.querySelector("li")
+listItem[12].style.opacity = "0.3"
+listItem[2].innerHTML = "Greetings!"
+listItem[16].innerHTML = "Never gonna give you up."
